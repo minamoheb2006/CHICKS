346888595acd3ca0ef4bdd8975cd9f854b72fa1c
mina moheb <minamoheb2006@gmail.com>
2026-09-13 05:04:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/CHICKS/",
+
   plugins: [react(), tailwindcss()],
 
   resolve: {
@@ -12,9 +13,6 @@ export default defineConfig({
       "@": path.resolve(import.meta.dirname, "src"),
     },
   },
-
-  root: path.resolve(import.meta.dirname),
-  publicDir: path.resolve(import.meta.dirname, "public"),
 
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
